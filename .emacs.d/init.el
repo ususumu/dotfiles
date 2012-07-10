130c5f548c13166b7d42839cbb0c17990da2b936
susumu <susumu@SMBIOSVGC-LB52HB.(none)>
2012-07-11 02:29:45 +0900

    added paren function

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,5 +1,5 @@
 (global-linum-mode t)
-(setq-default- tab-width 4)
+;;(setq-default-tab-width 4)
 (defun add-to-load-path (&rest paths)
   (let (path))
     (dolist (path paths paths)
@@ -9,3 +9,10 @@
 	(if (fboundp 'normal-top-level-add-subdirs-to-load-path)
 	    (normal-top-level-add-subdirs-to-load-path)))))
 (add-to-load-path "elisp" "conf" "public_repos")
+(setq show-paren-delay 0);表示までの秒数
+(show-paren-mode t)
+;;parenのスタイル; expressionは括弧内も強調表示
+(setq show-paren-style 'expression)
+;;フェイスを変更する
+(set-face-background 'show-paren-match-face nil)
+(set-face-underline-p 'show-paren-match-face "yellow")
